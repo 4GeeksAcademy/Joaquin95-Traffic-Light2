b2db5f31e8dba119fd810d53b4d76487ba651f6a
Joaquin Morales <Mintinvestments95@gmail.com>
2024-10-03 21:54:42 +0000

    Completed

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,24 @@
 import React, { useState } from "react";
 
-//include images into your bundle
-
-//create your first component
 const Home = () => {
   const [color, setColor] = useState("red");
 
   return (
     <div className="traffic-light">
       <div
-       className={"light  red" + (color === "red" ? "glow" : "")}
-      onClick={() => setColor ("red")}></div>
+        className={`light red ${color === "red" ? "glow" : ""}`}
+        onClick={() => setColor("red")}
+      ></div>
 
-      <div 
-      className={"light  yellow" + (color === "yellow" ? "glow" : "")}
-      onClick={() => setColor ("yellow")}></div>
+      <div
+        className={`light yellow ${color === "yellow" ? "glow" : ""}`}
+        onClick={() => setColor("yellow")}
+      ></div>
 
-      <div 
-      className={"light  green" + (color === "green" ? "glow" : "")}
-      onClick={() => setColor ("green")}></div>
+      <div
+        className={`light green ${color === "green" ? "glow" : ""}`}
+        onClick={() => setColor("green")}
+      ></div>
     </div>
   );
 };
